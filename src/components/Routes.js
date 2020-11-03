c0242a707cf24daca8488c8336d02766cff9f2d0
juraffe <cd80@kakao.com>
2020-11-04 02:59:58 +0900

    🛣️ 매칭되지 않은 라우트 경로에 대한 처리

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -1,18 +1,22 @@
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
 import PropTypes from "prop-types";
 
 import Auth from "../Routes/Auth";
 import Monitor from "../Routes/Monitor";
+import Profile from "../Routes/Profile";
 
 const LoggedInRoutes = () => (
   <Switch>
     <Route exact path="/" component={Monitor} />
+    <Route path="/:username" component={Profile} />
+    <Redirect from="*" to="/" />
   </Switch>
 );
 
 const LoggedOutRoutes = () => (
   <Switch>
     <Route exact path="/" component={Auth} />
+    <Redirect from="*" to="/" />
   </Switch>
 );
 
