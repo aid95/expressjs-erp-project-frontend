56924471008a1ad0fe478bdb4aded2964437866a
juraffe <cd80@kakao.com>
2020-11-13 21:25:38 +0900

    🚀 Subscriptions 활성화를 위한 Apollo client 수정

--- src/Apollo/Client.js
+++ src/Apollo/Client.js
@@ -1,23 +1,72 @@
-import ApolloClient from "apollo-boost";
+import {
+  ApolloClient,
+  HttpLink,
+  ApolloLink,
+  InMemoryCache,
+  concat,
+  split,
+} from "@apollo/client";
+import { onError } from "apollo-link-error";
+import { withClientState } from "apollo-link-state";
+import { WebSocketLink } from "apollo-link-ws";
+import { getMainDefinition } from "apollo-utilities";
+
 import { defaults, resolvers } from "./LocalState";
 
+const stateLink = withClientState({
+  resolvers,
+  defaults,
+});
+
+const httpLink = new HttpLink({
+  uri: "http://localhost:4000/",
+});
+
+const authMiddleware = new ApolloLink((operation, forward) => {
+  // add the authorization to the headers
+  operation.setContext({
+    headers: {
+      authorization: `Bearer ${localStorage.getItem("token") || null}`,
+    },
+  });
+
+  return forward(operation);
+});
+
+const wsLink = new WebSocketLink({
+  uri: `ws://localhost:4000/`,
+  options: {
+    reconnect: true,
+  },
+});
+
+const splitLink = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    return (
+      definition.kind === "OperationDefinition" &&
+      definition.operation === "subscription"
+    );
+  },
+  wsLink,
+  concat(authMiddleware, httpLink)
+);
+
 const Client = new ApolloClient({
-  uri:
-    process.env.NODE_ENV === "development"
-      ? "http://localhost:4000"
-      : "http://temp/",
-  clientState: {
-    defaults,
-    resolvers,
-  },
-  request: (operation) => {
-    const token = localStorage.getItem("token");
-    operation.setContext({
-      headers: {
-        authorization: token ? `Bearer ${token}` : "",
-      },
-    });
-  },
+  link: ApolloLink.from([
+    onError(({ graphQLErrors, networkError }) => {
+      if (graphQLErrors)
+        graphQLErrors.map(({ message, locations, path }) =>
+          console.log(
+            `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
+          )
+        );
+      if (networkError) console.log(`[Network error]: ${networkError}`);
+    }),
+    stateLink,
+    splitLink,
+  ]),
+  cache: new InMemoryCache(),
 });
 
 export default Client;
