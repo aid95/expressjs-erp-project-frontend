00c5f6322f8d4225d8fde69987c4af6ce03c9732
juraffe <cd80@kakao.com>
2020-11-22 13:06:01 +0900

    📦 스타일 관련 패키지 및 CSS 추가

--- src/Styles/Theme.js
+++ src/Styles/Theme.js
@@ -18,6 +18,16 @@ const Theme = {
              border-radius:${BORDER_RADIUS};
              background-color:white;
             `,
+  modalStyles: {
+    content: {
+      top: "50%",
+      left: "50%",
+      right: "auto",
+      bottom: "auto",
+      marginRight: "-50%",
+      transform: "translate(-50%, -50%)",
+    },
+  },
 };
 
 export default Theme;
